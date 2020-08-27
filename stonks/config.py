4cfed29c8124ec8cd2487a24d41ee585a816a626
Killian Stacey <killianstacey5@gmail.com>
2020-08-27 12:32:50 -0700

    config: add lines for DB cxn

--- stonks/config.py
+++ stonks/config.py
@@ -8,13 +8,19 @@ Reading the key/value pair from the config object requires you to first index th
 Another way to od it is using the object method:
     e.g.: input: config.get('section', 'key') -> output: 'value of interest'
 All values read from the config.ini file are string so type conversion must be made for non-string information.
+
+When sections not found: configparser.NoSectionError
+When keys not found: configparser.NoOptionError
 """
+is_debug_mode = False
 
 from ast import literal_eval  # For evaluating tuples,lists from config file
 from pathlib import Path
 import configparser
 import os
+import sqlite3
 import sys
+import time
 
 from stonks import logger_config
 
@@ -27,37 +33,48 @@ if BASE_PROJECT_PATH not in sys.path: sys.path.insert(0, BASE_PROJECT_PATH)
 
 # Instantiate variables for later use
 config_file_name = 'config.ini'
-default_log_folder_path = Path(BASE_PROJECT_PATH, 'logs').absolute()
 default_log_file_name = 'default.log'
-
+default_log_folder_path = Path(BASE_PROJECT_PATH, 'logs').absolute()
+valid_db_cxns = {'MYSQL', 'SQLITE'}
 # Load up config file
 configuration = configparser.ConfigParser()
 configuration.read(os.path.join(BASE_PROJECT_PATH, config_file_name))
 
-# Read in singular config variables
 FINNHUB_TOKEN = configuration.get('FINNHUB', 'TOKEN', raw=True)
 
+
+########################################################################################################################
+
+# Instantiate DB cxn
+db_cxn_type = configuration.get('APP', 'DB_CONNECTION')
+sqlite_file_name = configuration.get('SQLITE', 'FILE_NAME')
+sqlite_save_path = configuration.get('SQLITE', 'LOCATION', fallback=None)
+if is_debug_mode:
+    print(f'sqlite_save_path = {sqlite_save_path}, type={type(sqlite_save_path)}')
+if not sqlite_save_path:
+    sqlite_save_path = BASE_PROJECT_PATH
+else:
+    if not os.path.isdir(sqlite_save_path):
+        raise ValueError(f'The following path is not valid: {sqlite_save_path}')
+cxn_string_sqlite3 = os.path.join(sqlite_save_path, sqlite_file_name)
 
 # Resolve logger variables
 config_file_log_folder_path = configuration.get('LOGGING', 'LOG_FILE_FOLDER_PATH')
 config_file_log_folder_path = config_file_log_folder_path if config_file_log_folder_path else default_log_folder_path
-
 config_file_name = configuration.get('LOGGING', 'LOG_FILE_NAME', fallback=default_log_file_name)
-
-log_file_file_path = str(Path(config_file_log_folder_path, config_file_name).absolute())
-
+log_file_complete_path = str(Path(config_file_log_folder_path, config_file_name).absolute())
 assert os.path.isdir(config_file_log_folder_path), f'Path does not exist: {config_file_log_folder_path}'
 
 # Instantiate logger
-bsoid_logger = logger_config.create_generic_logger(
-    logger_name=configuration.get('LOGGING', 'LOG_FILE_NAME'),
+logger = logger_config.create_generic_logger(
+    logger_name=configuration.get('LOGGING', 'LOGGER_NAME'),
     log_format=configuration.get('LOGGING', 'LOG_FORMAT', raw=True),
     stdout_log_level=configuration.get('LOGGING', 'STREAM_LOG_LEVEL', fallback=None),
     file_log_level=configuration.get('LOGGING', 'FILE_LOG_LEVEL', fallback=None),
-    file_log_file_path=log_file_file_path,
+    file_log_file_path=log_file_complete_path,
 )
 
 
 if __name__ == '__main__':
-    print(f'sys.path = {sys.path}')
+
     pass
