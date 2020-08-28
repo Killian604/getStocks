69cb2a1b3f2a1c8cba8f197acb6a202f0092199a
Killian Stacey <killianstacey5@gmail.com>
2020-08-27 21:23:48 -0700

    Add source table and data inst. to sqlite3 db instantiation

--- stonks/apps/instantiate_sqlite3_database.py
+++ stonks/apps/instantiate_sqlite3_database.py
@@ -6,12 +6,13 @@ import re
 import sqlite3
 
 import stonks.config as config
-import stonks.tables.sqlite as stonks_sqlite
+import stonks.database.sql.data_instantiate as data_instantiate
+import stonks.database.sql.sqlite_create as stonks_sqlite
 
 logger = config.logger
 
 
-def instantiate_sqlite_database_without_overwrite(cxn_string: str):
+def instantiate_sqlite_database_without_table_overwrite(cxn_string: str):
     """
     Central function for creating the SQL database for housing data.
     :param cxn_string: (str) The string used in sqlite3.connect(`cxn_string`) use to instantiate a db cxn.
@@ -28,7 +29,6 @@ def instantiate_sqlite_database_without_overwrite(cxn_string: str):
                 # Unexpected error
                 logger.error(f'Error in creating symbols table: {repr(op_err)}')
                 raise op_err
-
         # create candlesticks table
         try:
             cxn.execute(stonks_sqlite.sql_create_table_candlestick())
@@ -40,11 +40,32 @@ def instantiate_sqlite_database_without_overwrite(cxn_string: str):
                 # Unexpected error
                 logger.error(f'Error in creating symbols table: {repr(op_err)}')
                 raise op_err
+        # create source table
+        try:
+            cxn.execute(stonks_sqlite.sql_create_table_source())
+        except sqlite3.OperationalError as op_err:
+            if re.search(r"""OperationalError\('table "\w+" already exists'\)""", repr(op_err)):
+                # If result not None: table exists, do not overwrite
+                pass
+            else:
+                # Unexpected error
+                logger.error(f'Error in creating symbols table: {repr(op_err)}')
+                raise op_err
+
+        # Instantiate data
+        cxn.execute(data_instantiate.sql_insert_finnhub_to_source)
+        cxn.commit()
 
 
 def test_instant_sqlite_db() -> None:
-    instantiate_sqlite_database_without_overwrite(config.cxn_string_sqlite3)
+    instantiate_sqlite_database_without_table_overwrite(config.cxn_string_sqlite3)
     config.logger.info(f'Database instantiated to: {config.cxn_string_sqlite3}')
+
+
+def instantiate_database(cxn: object):
+    # TODO: make a connection-independent solution for prepping database from scratch
+
+    return
 
 
 if __name__ == '__main__':
